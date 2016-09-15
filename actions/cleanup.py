ab80b3ccd374918d19e0358bfe43638be6e89a9e
Louis Bouchard <louis.bouchard@canonical.com>
2016-09-15 15:22:49 +0200

    [actions] Rename actions with .py suffix and symlink
    In order to import actions as python modules to test, they
    need the .py suffix. Rename the action scripts and symlink
    so juju action is happy.
    
    Signed-off-by: Louis Bouchard <louis.bouchard@canonical.com>

---
+++ actions/cleanup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+from charmhelpers.core.hookenv import action_get
+from charmhelpers.core.hookenv import action_set
+from charmhelpers.core.hookenv import action_fail
+from charmhelpers.core.hookenv import DEBUG
+from charmhelpers.core.hookenv import log as juju_log
+import sys
+import os
+
+juju_home = '/home/ubuntu'
+params = action_get()
+
+if params:
+    if 'homedir' in params.keys():
+        if not os.path.isdir(params['homedir']):
+            action_set({'outcome': 'failure'})
+            action_msg = 'homedir: Invalid path - %s' % params['homedir']
+            action_fail(action_msg)
+            sys.exit(0)
+        juju_home = params['homedir']
+
+try:
+    juju_log("Cleaning up %s" % juju_home, level=DEBUG)
+    for (path, name, files) in os.walk(juju_home):
+        for sosfile in files:
+            if (os.path.exists(path + "/" + sosfile)
+                and sosfile.startswith('sosreport-')):
+                os.unlink(path + "/" + sosfile)
+    action_msg = 'Directory %s cleaned up' % juju_home
+    action_set({'result-map.message': action_msg})
+    action_set({'outcome': 'success'})
+    sys.exit(0)
+except Exception as Err:
+    action_set({'outcome': 'failure'})
+    action_msg = 'Unable to cleanup %s' % juju_home
+    action_fail(action_msg)
+    sys.exit(0)
---
+++ actions/collect.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+
+from charmhelpers.core.hookenv import action_get
+from charmhelpers.core.hookenv import action_set
+from charmhelpers.core.hookenv import action_fail
+from charmhelpers.core.hookenv import DEBUG
+from charmhelpers.core.hookenv import log as juju_log
+import sys
+import os
+import re
+import shutil
+from subprocess import check_output
+
+
+def has_enough_space(mydir, minfree, mingig):
+    '''
+    Verify if filesystem has more than minfree% of free space
+    available and that it represent more than mingig Gb
+    '''
+    # Set default values
+    gigabyte = 1073741824
+    if not minfree:
+        minfree = 5
+    if not mingig:
+        mingig = 1
+
+    dirstat = os.statvfs(mydir)
+    pctfree = int(dirstat.f_bavail / dirstat.f_blocks * 100)
+    if pctfree < minfree:
+        spc_avail = int(dirstat.f_bavail * dirstat.f_bsize / gigabyte)
+        if spc_avail < mingig:
+            return False
+    return True
+
+
+command = ['sosreport']
+default_option = ['--batch']
+juju_home = '/home/ubuntu'
+params = action_get()
+minfree = None
+mingig = None
+
+if params:
+    if 'minfree' in params.keys():
+        minfree = params['minfree']
+    else:
+        minfree = None
+    if 'mingig' in params.keys():
+        mingig = params['mingig']
+    else:
+        mingig = None
+
+    if 'homedir' in params.keys():
+        if not os.path.isdir(params['homedir']):
+            action_set({'outcome': 'failure'})
+            action_msg = 'homedir: Invalid path - %s' % params['homedir']
+            action_fail(action_msg)
+            sys.exit(0)
+        juju_home = params['homedir']
+
+if not has_enough_space(juju_home, minfree, mingig):
+    action_set({'outcome': 'failure'})
+    action_msg = 'Not enough space in %s (minfree: %d mingig: %d)' % (
+                 juju_home, minfree, mingig)
+    action_fail(action_msg)
+    sys.exit(0)
+
+try:
+    juju_log("Running %s %s" % (command, default_option), level=DEBUG)
+    sosrun = check_output(command + default_option, universal_newlines=True)
+    regex = re.compile(r' /tmp.*tar.*')
+    tarball = regex.findall(sosrun)[0].lstrip(' ')
+    md5sum = tarball + '.md5'
+    juju_log("Moving %s in %s" % (tarball, juju_home), level=DEBUG)
+    shutil.move(tarball, juju_home)
+    juju_log("Moving %s in %s" % (md5sum, juju_home), level=DEBUG)
+    shutil.move(md5sum, juju_home)
+    action_msg = '%s and %s available in %s' % (tarball.lstrip('/tmp/'),
+                                                md5sum.lstrip('/tmp/'),
+                                                juju_home)
+    action_set({'result-map.message': action_msg})
+    action_set({'outcome': 'success'})
+    sys.exit(0)
+except Exception as Err:
+    action_set({'outcome': 'failure'})
+    action_msg = 'Unable to run sosreport'
+    action_fail(action_msg)
+    sys.exit(0)
